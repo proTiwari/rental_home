0d28d77f151273cbfb2d5426d126fad665326aec
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2021-08-25 17:25:52 +0530

    nav , search -  ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import NavBar from './components/NavBar';
+import SearchBar from './search/SearchBar';
 
 function App() {
   return (
     <div className="App">
- 
+      <NavBar />
+      <SearchBar />
     
     </div>
   );
