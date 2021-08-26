61cd69052ed9715126b97fb7b228ff2cdb0c37ef
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2021-08-26 16:50:12 +0530

    footer added. home page basic ui complete.

--- src/components/HomePageComponents/RandomVideoSugessions.js
+++ src/components/HomePageComponents/RandomVideoSugessions.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import { CardActions } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         minWidth: 275,
         margin: 5,
-        height: 400
+        height: 805
+    },
+
+    leg: {
+        minWidth: 268,
+        height:400
     }
 }));
 
@@ -17,19 +24,120 @@ function RandomVideoSugessions() {
     const classes = useStyles();
     return (
         <div>
-             <Card className={classes.root} variant="outlined">
-                <CardContent style={{ margin: '0rem' }} >
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                   
-                </CardContent>
-            </Card>
+
+                    
+            <div style={{ margin: "3rem" }} className={classes.root} variant="outlined">
+                <br/><br/>
+            <Typography variant="h4" >Some of our latest rental services</Typography>
+            <br/>
+                <div style={{
+                    display: "flex", flexDirection: "row",
+                    flexWrap: "wrap"
+                }}>
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card>
+
+
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+
+                    <Card className={classes.leg} variant="outlined">
+                        <CardContent>
+
+                        </CardContent>
+                        <CardActions>
+                            <Button></Button>
+                        </CardActions>
+                    </Card>
+
+                </div>
+                <br /><br />
+            </div>
         </div>
     )
 }
