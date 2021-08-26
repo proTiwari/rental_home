0fb16be879b208163cd1fe35a271e2ae03ac94d0
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2021-08-26 15:07:54 +0530

    some structure change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
-import SearchBar from '../search/SearchBar';
+import SearchBar from './SearchBar';
 
 const useStyles = makeStyles((theme) => ({
     grow: {
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import SearchBar from '../search/SearchBar';
+import SearchBar from '../components/SearchBar';
 
 function HomePage() {
     return (
