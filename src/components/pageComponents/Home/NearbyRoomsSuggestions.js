631b5a1081f0d5d17c2613c29ec89ee869c14faa
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2021-08-27 12:03:28 +0530

    horizontal scroll feature added

--- src/components/pageComponents/Home/NearbyRoomsSuggestions.js
+++ src/components/pageComponents/Home/NearbyRoomsSuggestions.js
@@ -4,15 +4,20 @@ import { CardActions, makeStyles, Typography, Button, CardContent, Card } from '
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        marginTop: 2,
         minWidth: 275,
-        margin: 3,
+        margin: 0,
     },
 
     leg: {
-        minWidth: 268,
-        height:300,
-        marginLeft: 10,
-        marginTop:50
+
+        display: 'inlineBlock',
+        color: 'white',
+        textAlign: 'center',
+        marginTop: '1rem',
+        textDecoration: 'none',
+        width: '20rem',
+        height: '23rem'
     }
 }));
 
@@ -20,85 +25,73 @@ function NearbyRoomsSuggestions() {
     const classes = useStyles();
     return (
 
-                    
-            <div className={classes.root} >
+
+        <div className={classes.root} >
             <Typography variant="h4" >Rooms available near you.</Typography>
-               <div style={{
-                    display: "flex", flexDirection: "row",
-                    flexWrap: "wrap"
-                }}>
+            <div style={{
+                overflow: 'auto',
+                whiteSpace: 'nowrap',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
 
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
 
-                       </CardContent>
-                       <CardActions></CardActions>
-                       <Button></Button>
-                   </Card>
+                <div style={{ overflowY: 'hidden', display: 'flex' }} class="scrollmenu">
+                    <div className={classes.leg} ><Card className={classes.leg} variant="outlined">
+                        <CardContent>
+                            <Button>click here</Button><Typography>this is typography</Typography>
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card></div>
+                    <div className={classes.leg} ><Card className={classes.leg} variant="outlined">
+                        <CardContent>
 
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card></div>
+                    <div className={classes.leg} ><Card className={classes.leg} variant="outlined">
+                        <CardContent>
 
-                       </CardContent>
-                       <CardActions>
-                           <Button></Button>
-                       </CardActions>
-                   </Card>
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card></div>
+                    <div className={classes.leg} ><Card className={classes.leg} variant="outlined">
+                        <CardContent>
 
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card></div>
+                    <div className={classes.leg} ><Card className={classes.leg} variant="outlined">
+                        <CardContent>
 
-                       </CardContent>
-                       <CardActions>
-                           <Button></Button>
-                       </CardActions>
-                   </Card>
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card></div>
+                    <div className={classes.leg} ><Card className={classes.leg} variant="outlined">
+                        <CardContent>
 
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card></div>
 
-                       </CardContent>
-                       <CardActions>
-                           <Button></Button>
-                       </CardActions>
-                   </Card>
-
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
-
-                       </CardContent>
-                       <CardActions></CardActions>
-                       <Button></Button>
-                   </Card>
-
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
-
-                       </CardContent>
-                       <CardActions>
-                           <Button></Button>
-                       </CardActions>
-                   </Card>
-
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
-
-                       </CardContent>
-                       <CardActions>
-                           <Button></Button>
-                       </CardActions>
-                   </Card>
-
-                   <Card className={classes.leg} variant="outlined">
-                       <CardContent>
-
-                       </CardContent>
-                       <CardActions>
-                           <Button></Button>
-                       </CardActions>
-                   </Card>
+                    <div className={classes.leg} ><Card className={classes.leg} variant="outlined">
+                        <CardContent>
+                            <Button>See More</Button>
+                        </CardContent>
+                        <CardActions></CardActions>
+                        <Button></Button>
+                    </Card></div>
 
                 </div>
+
+
+            </div>
         </div>
     )
 }
